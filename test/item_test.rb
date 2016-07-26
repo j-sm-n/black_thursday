fc32e694865540b06d1548f7ea7f7cd020bfc52f
PlanetEfficacy <jspevack@gmail.com>
2016-07-25 21:24:11 -0600

    Changed merchant and item id in merchant and item to be integers, not strings

--- test/item_test.rb
+++ test/item_test.rb
@@ -15,8 +15,8 @@ class ItemTest < Minitest::Test
       :unit_price  => BigDecimal.new(10.99,4),
       :created_at  => this_time_1,
       :updated_at  => this_time_2,
-      :id => "263444697",
-      :merchant_id => "12334496"
+      :id => 263444697,
+      :merchant_id => 12334496
     })
   end
 
@@ -25,13 +25,13 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_has_all_the_properties_of_an_item
-    assert_equal "263444697", @i.id
+    assert_equal 263444697, @i.id
     assert_equal "Pencil", @i.name
     assert_equal "You can use it to write things", @i.description
     assert_equal BigDecimal.new(10.99,4), @i.unit_price
     assert_equal this_time_1, @i.created_at
     assert_equal this_time_2, @i.updated_at
-    assert_equal "12334496", @i.merchant_id
+    assert_equal 12334496, @i.merchant_id
   end
 
   def test_it_can_return_unit_price_as_float
