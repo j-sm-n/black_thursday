e3da65bedf72374b1420ab18efc5fe9177119770
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-28 15:36:06 -0600

    closes 94 standard deviation of item count per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,41 +2,38 @@ require_relative "../lib/sales_engine"
 require 'pry'
 
 class SalesAnalyst
-  attr_reader :item_repo,
-              :merchant_repo,
-              :merchant_array,
-              :item_array
-
-  attr_accessor :items_per_merchant
+  attr_reader :merchants,
+              :items,
+              :item_count_per_merchant
 
   def initialize(sales_engine)
-    @merchant_repo = sales_engine.merchants # returns instance of MR
-    @items_per_merchant = [] # to collect number of items per merchant to calculate average
-    @merchants = merchant_repo.repository
-    @item_repo = sales_engine.items
-    @items = item_repo.repository
+    @merchants = sales_engine.merchants.repository
+    @item_count_per_merchant = find_how_many_items_merchants_sell # to collect number of items per merchant to calculate average
+    @items = sales_engine.items.repository
   end
 
   def find_how_many_items_merchants_sell
-    @items_per_merchant = @merchants.map do |merchant|
-      merchant_items = merchant.items.length
+    @merchants.map do |merchant|
+      merchant.items.length
     end
   end
 
-  def average_items_per_merchant
-    items_count = find_how_many_items_merchants_sell.count
-    items_sum = find_how_many_items_merchants_sell.reduce(:+)
-    average = (items_sum/items_count).to_f
+  def average_item_count_per_merchant #=> mean
+    (item_count_per_merchant.reduce(:+)/item_count_per_merchant.count).to_f
   end
 
   def find_variance_of_items_sold
-    @items_per_merchant.map do |items|
-      (((items - average_items_per_merchant) ** 2) / @items_per_merchant.length)
+    item_count_per_merchant.map do |item_count|
+      ((item_count - average_item_count_per_merchant) ** 2) / item_count_per_merchant.length
     end.reduce(:+)
   end
 
-  def average_items_per_merchant_standard_deviation
+  def average_items_per_merchant_standard_deviation #=> st deviation
     Math.sqrt(find_variance_of_items_sold)
   end
+
+  # def merchants_with_high_item_count
+  #   # array of merchants who sell more than mean + standard deviation
+  # end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -20,8 +20,8 @@ class SalesAnalystTest < Minitest::Test
                          })
     sales_analyst = SalesAnalyst.new(sales_engine)
 
-    assert_equal 9, sales_analyst.item_repo.count
-    assert_equal "510+ RealPush Icon Set", sales_analyst.item_repo.repository[0].name
+    assert_equal 9, sales_analyst.items.count
+    assert_equal "510+ RealPush Icon Set", sales_analyst.items[0].name
   end
 
   def test_it_has_access_to_all_merchants
@@ -31,8 +31,8 @@ class SalesAnalystTest < Minitest::Test
                          })
     sales_analyst = SalesAnalyst.new(sales_engine)
 
-    assert_equal 3, sales_analyst.merchant_repo.count
-    assert_equal "jejum", sales_analyst.merchant_repo.repository[0].name
+    assert_equal 3, sales_analyst.merchants.count
+    assert_equal "jejum", sales_analyst.merchants[0].name
   end
 
   def test_it_can_find_how_many_items_merchants_sell
@@ -52,7 +52,7 @@ class SalesAnalystTest < Minitest::Test
                          })
     sales_analyst = SalesAnalyst.new(sales_engine)
 
-    assert_equal 3.0, sales_analyst.average_items_per_merchant
+    assert_equal 3.0, sales_analyst.average_item_count_per_merchant
   end
 
   def test_it_finds_the_variance_of_items
@@ -76,5 +76,15 @@ class SalesAnalystTest < Minitest::Test
     sales_analyst.find_variance_of_items_sold
 
     assert_equal 1.632993161855452, sales_analyst.average_items_per_merchant_standard_deviation
+  end
+
+  def test_can_produce_list_of_merchants_who_sell_like_ballers
+    sales_engine = SalesEngine.from_csv({
+                           :items     => "./test/fixtures/sales_analyst_items_for_finding_average.csv",
+                           :merchants => "./test/fixtures/sales_analyst_merchants_for_finding_average.csv",
+                         })
+    sales_analyst = SalesAnalyst.new(sales_engine)
+    sales_analyst.find_how_many_items_merchants_sell
+    sales_analyst.find_variance_of_items_sold
   end
 end
