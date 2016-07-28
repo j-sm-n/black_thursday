79a0fb2da083fd7c9ea70e6b2ff6057dd4717494
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-28 10:05:18 -0600

    sales analyst knows about all items and all merchants

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,7 +2,11 @@ require_relative "../lib/sales_engine"
 require 'pry'
 
 class SalesAnalyst
-  def initialize(sales_engine)
+  attr_reader :item_repo,
+              :merchant_repo
 
+  def initialize(sales_engine)
+    @item_repo = sales_engine.items
+    @merchant_repo = sales_engine.merchants
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -13,4 +13,25 @@ class SalesAnalystTest < Minitest::Test
     assert_instance_of SalesAnalyst, sales_analyst
   end
 
+  def test_it_has_access_to_all_items
+    sales_engine = SalesEngine.from_csv({
+                           :items     => "./data/items_test.csv",
+                           :merchants => "./data/merchants_test.csv",
+                         })
+    sales_analyst = SalesAnalyst.new(sales_engine)
+
+    assert_equal 10, sales_analyst.item_repo.count
+    assert_equal "Handpainted HP Needlepoint Canvas &quot;Beautiful Shabbat&quot; Bracha Lavee BL32TB Judaica NWT! 211 dollars", sales_analyst.item_repo.repository[0].name
+  end
+
+  def test_it_has_access_to_all_merchants
+    sales_engine = SalesEngine.from_csv({
+                           :items     => "./data/items_test.csv",
+                           :merchants => "./data/merchants_test.csv",
+                         })
+    sales_analyst = SalesAnalyst.new(sales_engine)
+
+    assert_equal 9, sales_analyst.merchant_repo.count
+    assert_equal "Shopin1901", sales_analyst.merchant_repo.repository[0].name
+  end
 end
