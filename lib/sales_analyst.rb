2119f2e77dff2c04aa1fe1a2e61326792efd6881
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-28 11:08:20 -0600

    sales analyst can determine how many items merchants sell - closes #28

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -3,10 +3,24 @@ require 'pry'
 
 class SalesAnalyst
   attr_reader :item_repo,
-              :merchant_repo
+              :merchant_repo,
+              :merchant_array,
+              :item_array
+
+  attr_accessor :items_per_merchant
 
   def initialize(sales_engine)
+    @merchant_repo = sales_engine.merchants # returns instance of MR
+    @items_per_merchant = [] # to collect number of items per merchant to calculate average
+    @merchant_array = merchant_repo.repository
     @item_repo = sales_engine.items
-    @merchant_repo = sales_engine.merchants
+    @item_array = item_repo.repository
+  end
+
+  def find_how_many_items_merchants_sell
+    @items_per_merchant = @merchant_array.map do |merchant|
+      merchant_item_array = merchant.items
+      merchant_item_array.length
+    end
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -30,8 +30,18 @@ class SalesAnalystTest < Minitest::Test
                            :merchants => "./data/merchants_test.csv",
                          })
     sales_analyst = SalesAnalyst.new(sales_engine)
-
     assert_equal 9, sales_analyst.merchant_repo.count
     assert_equal "Shopin1901", sales_analyst.merchant_repo.repository[0].name
+  end
+
+  def test_it_can_find_how_many_items_merchants_sell
+    sales_engine = SalesEngine.from_csv({
+                           :items     => "./data/items_test.csv",
+                           :merchants => "./data/merchants_test.csv",
+                         })
+    sales_analyst = SalesAnalyst.new(sales_engine)
+
+    assert_equal [0, 0, 0, 0, 0, 0, 0, 0, 0], sales_analyst.find_how_many_items_merchants_sell
+    # returns array of the number of items each merchant in a data set sells
   end
 end
