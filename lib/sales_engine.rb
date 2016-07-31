583f5064214949967d6b4935276f3911f1be025c
PlanetEfficacy <jspevack@gmail.com>
2016-07-31 11:00:26 -0600

    updated sales engine to accomodate customer repo

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,6 +3,7 @@ require_relative '../lib/merchant_repository'
 require_relative '../lib/invoice_repository'
 require_relative '../lib/invoice_item_repository'
 require_relative '../lib/transaction_repository'
+require_relative '../lib/customer_repository'
 require_relative '../lib/loader'
 
 class SalesEngine
@@ -15,16 +16,16 @@ class SalesEngine
 
   def initialize(item_path, merchant_path, invoice_path, invoice_item_path,
                  transaction_path, customer_path)
-    @items     = ItemRepository.new(Loader.load(item_path), self)
-    @merchants = MerchantRepository.new(Loader.load(merchant_path), self)
-    @invoices  = InvoiceRepository.new(Loader.load(invoice_path), self)
+    @items         = ItemRepository.new(Loader.load(item_path), self)
+    @merchants     = MerchantRepository.new(Loader.load(merchant_path), self)
+    @invoices      = InvoiceRepository.new(Loader.load(invoice_path), self)
     @invoice_items = InvoiceItemRepository.new
-    @transactions = TransactionRepository.new
-    @customers = # CustomerRepository.new
+    @transactions  = TransactionRepository.new
+    @customers     = CustomerRepository.new
 
     invoice_items.from_csv(invoice_item_path, self)
     transactions.from_csv(transaction_path, self)
-    # customers.from_csv(customer_path, self)
+    customers.from_csv(customer_path, self)
   end
 
   def self.from_csv(hash_of_file_paths)
