6b14d8494e0841c0b69dcc1e5bed594a66d3aa57
PlanetEfficacy <jspevack@gmail.com>
2016-08-01 11:12:12 -0600

    first commit

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -29,36 +29,4 @@ class SalesAnalyst
   def invoices
     sales_engine.invoices
   end
-  # # InvoiceAnalyst
-  # def number_of_invoices_per_status
-  #   invoices.repository.map do |invoice|
-  #     invoice.status
-  #   end
-  #   # Refactored...
-  #   # invoices.repository.group_by { |invoice| invoice.status}
-  #   # {:shipped => [invoice1, invoice2, invoice3]}
-  # end
-  # # InvoiceAnalyst
-  # def invoice_status_counts
-  #   status_counts = Hash.new(0)
-  #   number_of_invoices_per_status.each do |status|
-  #     status_counts[status] += 1
-  #   end
-  #   status_counts
-  # end
-  # # Refactored...
-  # # number_of_invoices_per_status.each { |status| status.value.count }
-  #
-  # # InvoiceAnalyst
-  # def total_invoices
-  #   invoices.repository.count
-  # end
-  #
-  # # InvoiceAnalyst
-  # def invoice_status(status)
-  #   number = invoice_status_counts[status]
-  #   MathEngine.percentage(number, total_invoices)
-  # end
-  # # Refactored...
-  # # MathEngine.percentage(invoice_status_counts[status], total_invoices)
 end
