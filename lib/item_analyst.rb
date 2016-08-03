26eb8aa0cfde2a90f3b107460bfa1257b060d201
PlanetEfficacy <jspevack@gmail.com>
2016-08-03 10:07:43 -0600

    finds sold item for merchant

--- lib/item_analyst.rb
+++ lib/item_analyst.rb
@@ -38,11 +38,17 @@ module ItemAnalyst
     invoice_items[invoice_items.keys.max]
   end
 
-  # def max_revenue_invoice_items()
-  # end
-  #
-  # def most_sold_item_for_merchant(merchant_id)
-  # end
+  def max_revenue_invoice_items(merchant_id)
+    invoice_items = group_invoice_items_by_revenue(merchant_id)
+    invoice_items[invoice_items.keys.max]
+  end
+
+  def most_sold_item_for_merchant(merchant_id)
+    max_quantity_invoice_items(merchant_id).map do |invoice_item|
+      items.find_by_id(invoice_item.item_id)
+    end
+  end
+  
   # def best_item_for_merchant(merchant_id)
   # end
   # get all the invoices of a merchant that are paid in full
@@ -52,45 +58,45 @@ module ItemAnalyst
   # look up each item
 
 
-  def merchant_invoices_that_are_not_returned(merchant_id)
-    merchants.find_by_id(merchant_id).invoices.find_all do |invoice|
-      invoice.shipped? || invoice.pending? #Maybe should just be shipped?
-    end
-  end
-
-  def invoice_items_of_invoice_quantities(invoice_id)
-    invoice_items.find_all_by_invoice_id(invoice_id).group_by do |invoice_item|
-      invoice_item.quantity
-    end
-  end
-
-  def highest_quantity_invoice_items_by_invoice(invoice_id)
-    grouped_invoice_items = invoice_items_of_invoice_quantities(invoice_id)
-    just_invoice_items = grouped_invoice_items[grouped_invoice_items.keys.max]
-    why_do_I_take_one_invoice_item = [just_invoice_items[0]]
-  end
-
-  def get_all_highest_quantity_invoice_items_of_merchant(merchant_id)
-    merchant_invoices_that_are_not_returned(merchant_id).map do |invoice|
-      highest_quantity_invoice_items_by_invoice(invoice.id)
-    end.flatten
-  end
-
-  def group_highest_quantity_invoice_items_by_quantity(merchant_id)
-    get_all_highest_quantity_invoice_items_of_merchant(merchant_id).group_by do |invoice_item|
-      invoice_item.quantity
-    end
-  end
-
-  def highest_quantity_invoice_items(merchant_id)
-    grouped_invoice_items = group_highest_quantity_invoice_items_by_quantity(merchant_id)
-    grouped_invoice_items[grouped_invoice_items.keys.max]
-  end
-
-  def most_sold_item_for_merchant(merchant_id)
-    highest_quantity_invoice_items(merchant_id).map do |invoice_item|
-      items.find_by_id(invoice_item.item_id)
-    end
-  end
+  # def merchant_invoices_that_are_not_returned(merchant_id)
+  #   merchants.find_by_id(merchant_id).invoices.find_all do |invoice|
+  #     invoice.shipped? || invoice.pending? #Maybe should just be shipped?
+  #   end
+  # end
+  #
+  # def invoice_items_of_invoice_quantities(invoice_id)
+  #   invoice_items.find_all_by_invoice_id(invoice_id).group_by do |invoice_item|
+  #     invoice_item.quantity
+  #   end
+  # end
+  #
+  # def highest_quantity_invoice_items_by_invoice(invoice_id)
+  #   grouped_invoice_items = invoice_items_of_invoice_quantities(invoice_id)
+  #   just_invoice_items = grouped_invoice_items[grouped_invoice_items.keys.max]
+  #   why_do_I_take_one_invoice_item = [just_invoice_items[0]]
+  # end
+  #
+  # def get_all_highest_quantity_invoice_items_of_merchant(merchant_id)
+  #   merchant_invoices_that_are_not_returned(merchant_id).map do |invoice|
+  #     highest_quantity_invoice_items_by_invoice(invoice.id)
+  #   end.flatten
+  # end
+  #
+  # def group_highest_quantity_invoice_items_by_quantity(merchant_id)
+  #   get_all_highest_quantity_invoice_items_of_merchant(merchant_id).group_by do |invoice_item|
+  #     invoice_item.quantity
+  #   end
+  # end
+  #
+  # def highest_quantity_invoice_items(merchant_id)
+  #   grouped_invoice_items = group_highest_quantity_invoice_items_by_quantity(merchant_id)
+  #   grouped_invoice_items[grouped_invoice_items.keys.max]
+  # end
+  #
+  # def most_sold_item_for_merchant(merchant_id)
+  #   highest_quantity_invoice_items(merchant_id).map do |invoice_item|
+  #     items.find_by_id(invoice_item.item_id)
+  #   end
+  # end
 
 end
--- test/item_analyst_test.rb
+++ test/item_analyst_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 require './lib/sales_analyst'
 require 'pry'
 
-class SalesAnalystTest < Minitest::Test
+class ItemAnalystTest < Minitest::Test
   attr_reader :test_sales_analyst
 
   def setup
@@ -69,30 +69,31 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 263431273, invoice_items.first.item_id
   end
 
-  def test_it_can_find_all_non_returned_invoices_for_merchant
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
+  def test_it_can_find_the_max_revenue_invoice_items
     id = 12337105
-    expected_invoice_ids = [108, 145, 481, 728, 2878, 3074, 3950, 4878]
-    invoices = test_sales_analyst.merchant_invoices_that_are_not_returned(id)
-
-    assert_equal 8, invoices.length
-    invoices.each do |invoice|
-      assert_equal true, expected_invoice_ids.include?(invoice.id)
-    end
+    invoice_items = test_sales_analyst.max_revenue_invoice_items(id)
+    assert_equal 1, invoice_items.length
+    assert_equal 263463003, invoice_items.first.item_id
   end
 
-  # def test_it_can_group_invoice_items_by_quantity
+  def test_it_can_find_most_sold_item_for_merchant
+    merchant_id = 12337105
+    items = test_sales_analyst.most_sold_item_for_merchant(merchant_id)
+    assert_equal 4, items.length
+    assert_instance_of Item, items.first
+    assert_equal 263431273, items.first.id
+  end
+
+  def test_it_can_find_best_item_for_merchant
+    skip
+    merchant_id = 12337105
+    items = test_sales_analyst.most_sold_item_for_merchant(merchant_id)
+    assert_equal 1, items.length
+    assert_instance_of Item, items.first
+    assert_equal 263463003, items.first.id
+  end
+
+  # def test_it_can_find_all_non_returned_invoices_for_merchant
   #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
   #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
   #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
@@ -105,128 +106,151 @@ class SalesAnalystTest < Minitest::Test
   #   test_sales_engine = SalesEngine.from_csv(file_paths)
   #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
   #
-  #   id = 2878
-  #   expected_quantities = [10, 8, 3, 1]
-  #   actual_quantities = test_sales_analyst.invoice_items_of_invoice_quantities(id).keys
+  #   id = 12337105
+  #   expected_invoice_ids = [108, 145, 481, 728, 2878, 3074, 3950, 4878]
+  #   invoices = test_sales_analyst.merchant_invoices_that_are_not_returned(id)
   #
-  #   assert_equal expected_quantities, actual_quantities
+  #   assert_equal 8, invoices.length
+  #   invoices.each do |invoice|
+  #     assert_equal true, expected_invoice_ids.include?(invoice.id)
+  #   end
   # end
-
-  def test_it_knows_highest_quantity_invoice_item_by_invoice
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
-    invoice_id_1 = 108
-    invoice_item_id_1 = 508
-
-    invoice_id_2 = 2878
-    invoice_item_id_2 = 12791
-    # invoice_item_id_2 = [12791, 12795]
-
-    invoice_id_3 = 4878
-    invoice_item_id_3 = 21723
-
-    expected_1 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_1)
-    expected_2 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2)
-    # expected_2 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2[0])
-    # expected_3 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2[1])
-    expected_3 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_3)
-
-    actual_1 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_1)
-    # binding.pry
-    actual_2 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_2)
-    actual_3 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_3)
-
-    assert_equal expected_1, actual_1.first
-    assert_equal expected_2, actual_2.first
-    assert_equal expected_3, actual_3.first
-    # assert_equal expected_4, actual_3.first
-  end
-
-  def test_it_gets_all_highest_quantity_invoice_items_of_merchant
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
-    invoice_items = test_sales_analyst.get_all_highest_quantity_invoice_items_of_merchant(12337105)
-    assert 9, invoice_items.length
-  end
-
-  def test_it_gets_all_highest_quantity_invoice_items_of_merchant
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
-    grouping = test_sales_analyst.group_highest_quantity_invoice_items_by_quantity(12337105)
-    assert_equal [10, 9, 7, 8, 1], grouping.keys
-  end
-
-  def test_it_finds_highest_quantity_invoice_items
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
-    invoice_items = test_sales_analyst.highest_quantity_invoice_items(12337105)
-    assert_equal 4, invoice_items.length
-  end
-
-  def test_it_knows_most_items_sold_for_merchant
-    invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
-    merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
-    invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
-    item_path = "./data/items.csv"
-    file_paths = {:merchants => merchant_path,
-                  :items => item_path,
-                  :invoice_items => invoice_item_path,
-                  :invoices => invoice_path}
-
-    test_sales_engine = SalesEngine.from_csv(file_paths)
-    test_sales_analyst = SalesAnalyst.new(test_sales_engine)
-
-    items_1 = test_sales_analyst.most_sold_item_for_merchant(12334189)
-    items_2 = test_sales_analyst.most_sold_item_for_merchant(12334768)
-    items_3 =  test_sales_analyst.most_sold_item_for_merchant(12337105)
-
-    item_263524984 = items_1.find { |item| item.id == 263524984 }
-    item_263549386 = items_2.find { |item| item.id == 263549386 }
-
-    assert_equal 263524984, item_263524984.id
-    assert_equal 263549386, item_263549386.id
-    # binding.pry
-    assert_equal 4, items_3.length
-  end
+  #
+  # # def test_it_can_group_invoice_items_by_quantity
+  # #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  # #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  # #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  # #   item_path = "./data/items.csv"
+  # #   file_paths = {:merchants => merchant_path,
+  # #                 :items => item_path,
+  # #                 :invoice_items => invoice_item_path,
+  # #                 :invoices => invoice_path}
+  # #
+  # #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  # #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  # #
+  # #   id = 2878
+  # #   expected_quantities = [10, 8, 3, 1]
+  # #   actual_quantities = test_sales_analyst.invoice_items_of_invoice_quantities(id).keys
+  # #
+  # #   assert_equal expected_quantities, actual_quantities
+  # # end
+  #
+  # def test_it_knows_highest_quantity_invoice_item_by_invoice
+  #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  #   item_path = "./data/items.csv"
+  #   file_paths = {:merchants => merchant_path,
+  #                 :items => item_path,
+  #                 :invoice_items => invoice_item_path,
+  #                 :invoices => invoice_path}
+  #
+  #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  #
+  #   invoice_id_1 = 108
+  #   invoice_item_id_1 = 508
+  #
+  #   invoice_id_2 = 2878
+  #   invoice_item_id_2 = 12791
+  #   # invoice_item_id_2 = [12791, 12795]
+  #
+  #   invoice_id_3 = 4878
+  #   invoice_item_id_3 = 21723
+  #
+  #   expected_1 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_1)
+  #   expected_2 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2)
+  #   # expected_2 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2[0])
+  #   # expected_3 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_2[1])
+  #   expected_3 = test_sales_analyst.invoice_items.find_by_id(invoice_item_id_3)
+  #
+  #   actual_1 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_1)
+  #   # binding.pry
+  #   actual_2 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_2)
+  #   actual_3 = test_sales_analyst.highest_quantity_invoice_items_by_invoice(invoice_id_3)
+  #
+  #   assert_equal expected_1, actual_1.first
+  #   assert_equal expected_2, actual_2.first
+  #   assert_equal expected_3, actual_3.first
+  #   # assert_equal expected_4, actual_3.first
+  # end
+  #
+  # def test_it_gets_all_highest_quantity_invoice_items_of_merchant
+  #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  #   item_path = "./data/items.csv"
+  #   file_paths = {:merchants => merchant_path,
+  #                 :items => item_path,
+  #                 :invoice_items => invoice_item_path,
+  #                 :invoices => invoice_path}
+  #
+  #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  #
+  #   invoice_items = test_sales_analyst.get_all_highest_quantity_invoice_items_of_merchant(12337105)
+  #   assert 9, invoice_items.length
+  # end
+  #
+  # def test_it_gets_all_highest_quantity_invoice_items_of_merchant
+  #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  #   item_path = "./data/items.csv"
+  #   file_paths = {:merchants => merchant_path,
+  #                 :items => item_path,
+  #                 :invoice_items => invoice_item_path,
+  #                 :invoices => invoice_path}
+  #
+  #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  #
+  #   grouping = test_sales_analyst.group_highest_quantity_invoice_items_by_quantity(12337105)
+  #   assert_equal [10, 9, 7, 8, 1], grouping.keys
+  # end
+  #
+  # def test_it_finds_highest_quantity_invoice_items
+  #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  #   item_path = "./data/items.csv"
+  #   file_paths = {:merchants => merchant_path,
+  #                 :items => item_path,
+  #                 :invoice_items => invoice_item_path,
+  #                 :invoices => invoice_path}
+  #
+  #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  #
+  #   invoice_items = test_sales_analyst.highest_quantity_invoice_items(12337105)
+  #   assert_equal 4, invoice_items.length
+  # end
+  #
+  # def test_it_knows_most_items_sold_for_merchant
+  #   invoice_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice.csv"
+  #   merchant_path = "./test/fixtures/iteration04_sold_item_for_merchant_merchant.csv"
+  #   invoice_item_path = "./test/fixtures/iteration04_sold_item_for_merchant_invoice_item.csv"
+  #   item_path = "./data/items.csv"
+  #   file_paths = {:merchants => merchant_path,
+  #                 :items => item_path,
+  #                 :invoice_items => invoice_item_path,
+  #                 :invoices => invoice_path}
+  #
+  #   test_sales_engine = SalesEngine.from_csv(file_paths)
+  #   test_sales_analyst = SalesAnalyst.new(test_sales_engine)
+  #
+  #   items_1 = test_sales_analyst.most_sold_item_for_merchant(12334189)
+  #   items_2 = test_sales_analyst.most_sold_item_for_merchant(12334768)
+  #   items_3 =  test_sales_analyst.most_sold_item_for_merchant(12337105)
+  #
+  #   item_263524984 = items_1.find { |item| item.id == 263524984 }
+  #   item_263549386 = items_2.find { |item| item.id == 263549386 }
+  #
+  #   assert_equal 263524984, item_263524984.id
+  #   assert_equal 263549386, item_263549386.id
+  #   # binding.pry
+  #   assert_equal 4, items_3.length
+  # end
 
 end
